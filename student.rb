682c14c79af1db55e0495b5ad35226a42ae1583e
fitz95 <fitzgeraldnsong@gmail.com>
2023-04-12 10:13:08 +0100

    solve errors

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
 require_relative 'Person'
 
-class Student < Person 
-    def initialize(classroom)
-        super(id, age, name = "Unknown", parent_permission: true)
-        @classroom = classroom
-    end
+class Student < Person
+  def initialize(classroom)
+    super(id, age, parent_permission: true)
+    @classroom = classroom
+  end
 
-    def play_hooky
-       "¯\(ツ)/¯"
-    end
-end 
+  def play_hooky
+    '¯(ツ)/¯'
+  end
+end
--- teacher.rb
+++ teacher.rb
@@ -1,10 +1,10 @@
 class Teacher < Person
-    def initialize(classroom)
-        super(id, age, name = "Unknown", parent_permission: true, specialization)
-        @specialization = specialization
-    end
-    
-    def can_use_services
-        true
-    end
+  def initialize(specialization)
+    super(id, age, parent_permission: true)
+    @specialization = specialization
+  end
+
+  def can_use_services
+    true
+  end
 end
