69cf7de7419458c9eb121e411ef7e65132f04ca1
fitz95 <fitzgeraldnsong@gmail.com>
2023-04-13 04:43:14 +0100

    fix errors

--- decorator.rb
+++ decorator.rb
@@ -2,11 +2,11 @@ require_relative 'Nameable'
 require_relative 'Person'
 
 class Decorator < Nameable
-    def initialize(name)
-        @name = name
-    end
+  def initialize(name)
+    @name = name
+  end
 
-    def correct_name
-        @name.correct_name
-    end
+  def correct_name
+    @name.correct_name
+  end
 end
